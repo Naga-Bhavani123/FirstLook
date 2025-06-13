39d7d017b46696096019a9e40833fb6e449af415
awais <awaisn.offi@gmail.com>
2025-06-13 06:19:57 +0000

    videos

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
                 <source src='/trailer.mp4' />
             </video>
             <div class="trailerName">
-                <span>Y</span><span>o</span><span>u</span><span>r</span><span style={{ width: '20px', display: 'inline-block' }}></span><span>F</span>
+                <span>M</span><span>y</span><span style={{ width: '20px', display: 'inline-block' }}></span><span>F</span>
                 <span>a</span><span>u</span><span>l</span><span>t</span>
             </div>
 
